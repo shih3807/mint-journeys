2f0e23d4b37722e0ca29e0af177b6d2804427eb0
shih <Poetic0124@icloud.com>
2026-03-27 16:53:37 +0800

    chore: update seed data for new schema

--- backend/seed.py
+++ backend/seed.py
@@ -1,13 +1,9 @@
 from models.table_models import (
-    User,
-    Trip,
-    TripMember,
     Category,
     Currency,
-    Transaction,
-    ExchangeRate,
 )
 from database import SessionLocal
+from sqlalchemy import select
 
 
 def insert_test_data():
@@ -15,41 +11,42 @@ def insert_test_data():
     try:
         # 類別
         default_category_data = [
-            {"is_default": True, "name": "餐飲"},
-            {"is_default": True, "name": "住宿"},
-            {"is_default": True, "name": "交通"},
-            {"is_default": True, "name": "購物"},
-            {"is_default": True, "name": "娛樂"},
-            {"is_default": True, "name": "景點門票"},
-            {"is_default": True, "name": "其他"},
+            {"name": "餐飲"},
+            {"name": "住宿"},
+            {"name": "交通"},
+            {"name": "購物"},
+            {"name": "娛樂"},
+            {"name": "景點門票"},
+            {"name": "簽證保險"},
+            {"name": "其他"},
         ]
 
         for data in default_category_data:
-            exists = db.query(Category).filter_by(name=data["name"]).first()
+            exists = db.execute(select(Category).where(Category.name == data["name"]))
             if not exists:
                 db.add(Category(**data))
 
             # 貨幣
         default_currency_data = [
-            {"code": "TWD", "name": "新台幣", "symbol": "NT$"},
-            {"code": "USD", "name": "美元", "symbol": "$"},
-            {"code": "EUR", "name": "歐元", "symbol": "€"},
-            {"code": "JPY", "name": "日圓", "symbol": "¥"},
-            {"code": "GBP", "name": "英鎊", "symbol": "£"},
-            {"code": "KRW", "name": "韓元", "symbol": "₩"},
-            {"code": "HKD", "name": "港幣", "symbol": "HK$"},
-            {"code": "CNY", "name": "人民幣", "symbol": "¥"},
-            {"code": "SGD", "name": "新加坡幣", "symbol": "S$"},
-            {"code": "AUD", "name": "澳幣", "symbol": "A$"},
-            {"code": "CAD", "name": "加拿大幣", "symbol": "C$"},
-            {"code": "CHF", "name": "瑞士法郎", "symbol": "CHF"},
-            {"code": "THB", "name": "泰銖", "symbol": "฿"},
-            {"code": "MYR", "name": "馬來西亞令吉", "symbol": "RM"},
-            {"code": "VND", "name": "越南盾", "symbol": "₫"},
-            {"code": "NZD", "name": "紐西蘭幣", "symbol": "NZ$"},
+            {"code": "TWD", "name": "新台幣"},
+            {"code": "USD", "name": "美元"},
+            {"code": "EUR", "name": "歐元"},
+            {"code": "JPY", "name": "日圓"},
+            {"code": "GBP", "name": "英鎊"},
+            {"code": "KRW", "name": "韓元"},
+            {"code": "HKD", "name": "港幣"},
+            {"code": "CNY", "name": "人民幣"},
+            {"code": "SGD", "name": "新加坡幣"},
+            {"code": "AUD", "name": "澳幣"},
+            {"code": "CAD", "name": "加拿大幣"},
+            {"code": "CHF", "name": "瑞士法郎"},
+            {"code": "THB", "name": "泰銖"},
+            {"code": "MYR", "name": "馬來西亞令吉"},
+            {"code": "VND", "name": "越南盾"},
+            {"code": "NZD", "name": "紐西蘭幣"},
         ]
         for data in default_currency_data:
-            exists = db.query(Currency).filter_by(code=data["code"]).first()
+            exists = db.execute(select(Currency).where(Currency.code == data["code"]))
             if not exists:
                 db.add(Currency(**data))
 
