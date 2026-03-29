1e0c24b8806845e71ca1e646d33493dbe43ca029
shih <Poetic0124@icloud.com>
2026-03-29 11:56:18 +0800

    chore: update Mantine dependencies and add new components

--- frontend/mint-journeys/src/main.tsx
+++ frontend/mint-journeys/src/main.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.tsx';
+
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/charts/styles.css';
+import '@mantine/dropzone/styles.css';
+import '@mantine/notifications/styles.css';
+
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
-import '@mantine/notifications/styles.css';
+import { ModalsProvider } from '@mantine/modals';
+
 import theme from './styles/theme.tsx';
 import '@fontsource/merriweather';
 import '@fontsource/noto-serif-tc';
 
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
 const rootElement = document.getElementById('root');
+
+dayjs.extend(customParseFormat);
+
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <MantineProvider defaultColorScheme="light" theme={theme}>
-        <Notifications position="top-center" zIndex={1000} />
-        <App />
+        <ModalsProvider>
+          <Notifications position="top-center" zIndex={1000} />
+          <App />
+        </ModalsProvider>
       </MantineProvider>
     </React.StrictMode>
   );
